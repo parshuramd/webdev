589293f16729d8ab824799ab54bf9b5003824c47
Parshuram Dadas <dadasparshuram96@gmail.com>
2025-02-11 20:26:52 +0530

    js

---
+++ js/hello.js
@@ -0,0 +1,9 @@
+const data = {
+  'v-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzNDU2Nzg5MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg3NTM2MDAwLCJleHAiOjE3MTkwNzIwMDB9.XyZ9aBcDqG5Z8Nqf7G4WpQJgJXh6qVj0P0Z_TxxsLwA'
+}
+
+cy.request(({
+  Headers:{
+    'v-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzNDU2Nzg5MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg3NTM2MDAwLCJleHAiOjE3MTkwNzIwMDB9.XyZ9aBcDqG5Z8Nqf7G4WpQJgJXh6qVj0P0Z_TxxsLwA'
+  }
+}))
